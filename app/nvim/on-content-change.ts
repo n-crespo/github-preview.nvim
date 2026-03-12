853a01e543503dc3ed2ddadb91c6e1c5ac3193f8
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-12 01:41:48 -0700

    fix(nvim-events): skip notifications for non-file buffers

--- app/nvim/on-content-change.ts
+++ app/nvim/on-content-change.ts
@@ -37,9 +37,12 @@ export async function onContentChange(
             group: app.augroupId,
             desc: "Notify github-preview",
             command: `lua
-            local buffer = vim.api.nvim_get_current_buf()
-            local path = vim.api.nvim_buf_get_name(0)
-            vim.rpcnotify(${app.nvim.channelId}, "${NOTIFICATION}", buffer, path)`,
+            local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
+            if buftype == "" then
+                local buffer = vim.api.nvim_get_current_buf()
+                local path = vim.api.nvim_buf_get_name(0)
+                vim.rpcnotify(${app.nvim.channelId}, "${NOTIFICATION}", buffer, path)
+            end`,
         },
     ]);
 
